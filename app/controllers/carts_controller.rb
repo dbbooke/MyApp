08bf5f460b2e399f6f971a37392758f7c821cb03
Darren Booker <darrenbooker@Darrens-MacBook-Pro.local>
2018-03-17 12:42:46 -0700

    updated carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,7 +11,7 @@ class CartsController < ApplicationController
 	end
 
 	def destroy
-		@cart.destroy if @cart.id == session [:cart_id]
+		@cart.destroy if @cart.id == session[:cart_id]
 		session[:cart_id] = nil
 		redirect_to shop_url, notice: 'Your cart is emtpy'
 	end
